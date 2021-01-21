d3c0288455107c9ac75e531d59d5feaaffe2c1ec
Meet Patel <meetaiims@yahoo.com>
2021-01-21 16:14:14 -0600

    add MainContextProvider component, export Provider and Consumer

--- src/contexts/MainContext.js
+++ src/contexts/MainContext.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 
-const MainContext = React.createContext({
-    lang: [],
-    words: [],
-    setLang: () => {},
-    setWords: () => {}
-})
+const { Provider, Consumer } = React.createContext({
+  language: [],
+  words: [],
+  setLanguage: () => {},
+  setWords: () => {},
+});
+
+class MainContextProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: {},
+      words: [],
+    };
+  }
+
+  setLanguage = (language) => {
+    this.setState({
+      language,
+    });
+  };
+
+  setWords = (words) => {
+    this.setState({
+      words,
+    });
+  };
+
+  render() {
+    const value = {
+      language: this.state.language,
+      words: this.state.words,
+      setLanguage: this.setLanguage,
+      setWords: this.setWords,
+    };
+
+    return <Provider value={value}>{this.props.children}</Provider>;
+  }
+}
+
+export { MainContextProvider, Consumer as MainContextConsumer };
