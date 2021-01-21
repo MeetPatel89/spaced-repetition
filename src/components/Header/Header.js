8ca207fcef2fb9c92e60f02b9a4fe31bfbecbb2f
Meet Patel <meetaiims@yahoo.com>
2021-01-21 15:52:25 -0600

    change 'signup' to 'Sign up' to pass cypress test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -33,7 +33,7 @@ class Header extends Component {
       <nav>
         <Link to='/login'>Login</Link>
         {' '}
-        <Link to='/register'>Signup</Link>
+        <Link to='/register'>Sign up</Link>
       </nav>
     )
   }
