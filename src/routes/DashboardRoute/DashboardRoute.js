f205e3ca45f5dde5d13ecba2aeb3a06217664875
Meet Patel <meetaiims@yahoo.com>
2021-01-21 18:19:34 -0600

    render dashboard using fetchLanguage, setLanguage and setWords methods

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -3,16 +3,39 @@ import MainContext from '../../contexts/MainContext';
 import { fetchLanguage } from '../../services/main-service';
 
 class DashboardRoute extends Component {
-  
-  render() {
-    return (
-      <section>
-        implement and style me
-      </section>
-    );
+  componentDidMount() {
+    fetchLanguage().then((res) => {
+      this.context.setLanguage(res.language);
+      this.context.setWords(res.words);
+    });
   }
+
+  render() {
+    const wordsArr = this.context.words.map((word) => {
+      return (
+        <ul key={word.id} className='arr'>
+          <li>
+            <p>{word.original}</p> ✅: {word.correct_count} ❌:{' '}
+            {word.incorrect_count}
+          </li>
+        </ul>
+      );
+    });
+    return (
+      <section className='dashboard'>
+        <h2> {this.context.language.name} </h2>
+        <div className='stats'>
+          <h2>Total Score: {this.context.language.total_score} </h2>
+
+        </div>
+        <a href='/learn'>Start Practicing</a>
+        <h3> Words To Practice </h3>
+        <div className='word-list'>{wordsArr}</div>
+      </section>
+    )
+}
 }
 
-DashboardRoute.contextType = MainContext
+DashboardRoute.contextType = MainContext;
 
-export default DashboardRoute
+export default DashboardRoute;
