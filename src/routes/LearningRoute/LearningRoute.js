9b614faf3653f43a60f356e46a2b8f91d543fb32
Meet Patel <meetaiims@yahoo.com>
2021-01-21 20:40:33 -0600

    refactor learningRoute component with props from ProgressContext

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,14 +1,87 @@
 import React, { Component } from 'react';
 import ProgressContext from '../../contexts/ProgressContext';
+import { submitGuess, fetchWords } from '../../services/main-service';
+import Response from '../../components/Response/Response';
 
 class LearningRoute extends Component {
+  handleSubmit = (guess) => {
+    submitGuess(guess).then((result) => {
+      this.context.setPrevWord(this.context.nextWord);
+      this.context.clearError();
+      this.context.setTotalScore(result.totalScore);
+      this.context.setWordCorrectCount(result.wordCorrectCount);
+      this.context.setWordIncorrectCount(result.wordIncorrectCount);
+      this.context.setNextWord(result.nextWord);
+      this.context.setAnswer(result.answer);
+      this.context.setGuess(guess);
+      this.context.setIsCorrect(result.isCorrect);
+      this.context.setIsResultDisplayed(true);
+    });
+  };
+
+  componentDidMount() {
+    fetchWords()
+      .then((data) => {
+        if (!data) {
+          throw new Error('Something went wrong');
+        }
+        this.context.setNextWord(data.nextWord);
+        this.context.setTotalScore(data.totalScore);
+        this.context.setWordCorrectCount(data.wordCorrectCount);
+        this.context.setWordIncorrectCount(data.wordIncorrectCount);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+
   render() {
     return (
       <section>
-        implement and style me
+        {!this.context.isResultDisplayed ? (
+          <section>
+            <div className='translate-container'>
+              <h2 className='instructions-header'>Translate the word:</h2>
+              <span>{this.context.nextWord}</span>
+            </div>
+            <form
+              id='submit-form'
+              onSubmit={(e) => {
+                e.preventDefault();
+                this.handleSubmit(e.target.guessForm.value);
+              }}
+            >
+              <label htmlFor='learn-guess-input'>Translation</label>
+              <input
+                type='text'
+                name='guessForm'
+                id='learn-guess-input'
+                required
+              ></input>
+              <button type='submit'>Submit Answer</button>
+            </form>
+          </section>
+        ) : (
+          <Response />
+        )}
+        <div className='DisplayScore'>
+          <p className='total-score word-score-keeper'>
+            Total Score: {this.context.totalScore}
+          </p>
+        </div>
+        <p className='word-score-keeper'>
+          You have answered this word correctly {this.context.wordCorrectCount}{' '}
+          times.
+        </p>
+        <p className='word-score-keeper'>
+          You have answered this word incorrectly{' '}
+          {this.context.wordIncorrectCount} times.
+        </p>
       </section>
     );
   }
 }
+
+LearningRoute.contextType = ProgressContext;
 
 export default LearningRoute
