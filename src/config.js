2e3ba20d68cecd99e2d0afa34fcb11819a10cb1b
Meet Patel <meetaiims@yahoo.com>
2021-01-26 12:18:34 -0600

    refactor config for connecting with heroku db

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://peaceful-journey-72196.herokuapp.com/api',
+  /*
+  API_ENDPOINT: 'http://localhost:8000/api',
+  */
   TOKEN_KEY: 'blogful-client-auth-token',
 }
