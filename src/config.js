52753c2201a878835f9b5556faa8541d21144db8
Meet Patel <meetaiims@yahoo.com>
2021-01-27 10:05:07 -0600

    refactor config for production deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export default {
+  
   API_ENDPOINT: 'https://peaceful-journey-72196.herokuapp.com/api',
   /*
   API_ENDPOINT: 'http://localhost:8000/api',
   */
   TOKEN_KEY: 'blogful-client-auth-token',
-}
+};
